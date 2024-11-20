e6eda5ac4a3c4a355565fbb4030dfd06e810afcd
Mohammad Nadil <mdnadil50@gmail.com>
2024-11-20 23:03:25 +0600

    review responsive added

--- src/component/layer/ReviewCard.jsx
+++ src/component/layer/ReviewCard.jsx
@@ -4,14 +4,14 @@ import icon from "../../assets/star.svg";
 const ReviewCard = ({ image, name, star, review, className }) => {
   return (
     <div className={`p-[2px] mx-2  lg:mx-5 bg-gradient-to-b from-Gr-start to-Gr-end rounded-lg  ${className}`}>
-      <div className=" py-4 lg:py-11 px-[1.125rem] lg:px-12 bg-primary-bg rounded-lg flex flex-col gap-y-6">
+      <div className=" py-4 lg:py-11 px-[1.125rem] lg:px-12 bg-primary-bg rounded-lg flex flex-col gap-y-1.5 lg:gap-y-6">
 
         <div className="detail flex items-center gap-x-3">
           <div className="image border border-primary rounded-full">
             <img
               src={image}
               alt={name}
-              className=" w-7 lg:w-14 h-7 lg:h-14 border border-primary-bg rounded-full object-cover"
+              className=" w-10 lg:w-14 h-10 lg:h-14 border border-primary-bg rounded-full object-cover"
             />
           </div>
           <div className="nameStar">
